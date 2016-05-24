34d4225ccb915c6baa30ff8e9e1ccf10ecc29f1c
Luca Ciotti <luca.ciotti@gmail.com>
2016-05-24 16:16:24 +0200

    v 0.0.1 Make:Auth Init Config Bower - Gulp First Layout Admin new file:   app/Http/Controllers/HomeController.php modified:   app/Http/routes.php modified:   bower.json modified:   gulpfile.js new file:   resources/views/admin.blade.php new file:   resources/views/layouts/admin.blade.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function() {
+  return view('admin');
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,38 @@ var elixir = require('laravel-elixir');
  |
  */
 
+// elixir(function(mix) {
+//     mix.sass('app.scss');
+// });
+
+var bowerDir = './resources/assets/vendor/';
+
+var cssPaths = [
+    bowerDir + "AdminLTE/bootstrap/css/bootstrap.css",
+    bowerDir + "font-awesome/css/font-awesome.css",
+    bowerDir + "Ionicons/css/ionicons.css",
+    bowerDir + "AdminLTE/dist/css/AdminLTE.css",
+    bowerDir + "AdminLTE/dist/css/skins/skin-blue.min.css",
+    bowerDir + "AdminLTE/dist/css/skins/skin-black.min.css",
+    bowerDir + "AdminLTE/dist/css/skins/skin-green.min.css",
+    bowerDir + "AdminLTE/dist/css/skins/skin-red.min.css",
+];
+
+var jsPaths = [
+    bowerDir + "AdminLTE/plugins/jQuery/jQuery-2.2.0.min.js",
+    bowerDir + "AdminLTE/bootstrap/js/bootstrap.js",
+    bowerDir + "AdminLTE/dist/js/app.js",
+];
+
+var fontPaths = [
+    bowerDir + "font-awesome/fonts",
+    bowerDir + "AdminLTE/bootstrap/fonts",
+];
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles(cssPaths, 'public/css/app.css')
+        .scripts(jsPaths, 'public/js/vendor.js')
+        .copy('resources/assets/js/app.js', 'public/js/app.js')
+        .copy(fontPaths, 'public/fonts');
+
 });
